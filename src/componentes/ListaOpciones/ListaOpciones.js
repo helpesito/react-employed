471810f102ee10d7776ef5fa826d4f80f53fdd59
heibert <heibert728@gmail.com>
2023-09-10 09:17:14 -0500

    no2

---
+++ src/componentes/ListaOpciones/ListaOpciones.js
@@ -0,0 +1,26 @@
+import "./ListaOpciones.css"
+
+const ListaOpciones = () => {
+
+
+    const equipos = [
+        'Programación',
+        'Front End',
+        'Data Science',
+        'Devops',
+        'UX y Diseño',
+        'Móvil',
+        'Innovación y  Gestión'
+    ]
+
+    return <div className="lista_opciones">
+        <label>Equipos</label>
+            <select>
+                {equipos.map((equipo,index)=> {
+                    return <option key={index}>{equipo}</option>
+                })}
+            </select>
+    </div>
+}
+
+export default ListaOpciones;
