e0f49ab973d0dcd713d2888e3df27dae53efc79b
heibert <heibert728@gmail.com>
2023-09-27 16:13:39 -0500

    nada

--- src/componentes/Footer/index.jsx
+++ src/componentes/Footer/index.jsx
@@ -1,7 +1,7 @@
 import "./Footer.css"
 
 const Footer = () => {
-    return <footer className='footer' style={{ backgroundImage: "url(../../../public/img/footer.png)" }}>
+    return <footer className='footer' style={{ backgroundImage: "url(./img/footer.png)" }}>
         <div className='redes'>
             <a href='https://www.aluracursos.com/'>
                 <img src="/img/facebook.png" alt='Facebook' />
