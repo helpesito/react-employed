add95ac57e33f6ba58a4ef413dfaaaed364f7655
heibert <heibert728@gmail.com>
2023-09-27 14:54:21 -0500

    f2

--- src/componentes/Header/Header.js
+++ src/componentes/Header/Header.js
@@ -2,7 +2,7 @@ import './Header.css'
 
 function Header() {
     return <header className='header'>
-                <img src='/img/header.png' alt='org_header' />;
+                <img src='../../../public/img/header.png' alt='org_header' />
             </header>
         
 }
